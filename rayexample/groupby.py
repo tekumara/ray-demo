009f9e064ae706e1726c21b105cd73dfde890e13
Oliver Mannion <125105+tekumara@users.noreply.github.com>
2022-01-12 17:50:49 +1100

    feat: add examples

---
+++ rayexample/groupby.py
@@ -0,0 +1,10 @@
+import ray
+
+ds1 = ray.data.range(100).groupby(lambda x: x % 3).count()
+
+ds2 = ray.data.from_items([{"A": x % 3, "B": x} for x in range(100)])
+
+ds2.groupby("A").count().show()
+
+
+ray.data.from_items([(i % 3, i, i**2) for i in range(100)]).groupby(lambda x: x[0] % 3).sum(lambda x: x[2])
---
+++ rayexample/map_batches.py
@@ -0,0 +1,17 @@
+import ray
+
+# Create a dataset and then create a pipeline from it.
+base = ray.data.range(1000000)
+print(base)
+# -> Dataset(num_blocks=200, num_rows=1000000, schema=<class 'int'>)
+pipe = base.window(blocks_per_window=10)
+print(pipe)
+# -> DatasetPipeline(num_windows=20, num_stages=1)
+
+# Applying transforms to pipelines adds more pipeline stages.
+pipe = pipe.map_batches(lambda batch: [v * 2 for v in batch])
+print(pipe)
+
+num_rows = 0
+for row in pipe.iter_rows():
+    num_rows += 1
