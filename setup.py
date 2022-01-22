911298272c67eb2719a6cefe716be59fff546053
Oliver Mannion <125105+tekumara@users.noreply.github.com>
2022-01-22 18:47:03 +1100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="rayexample",
+    version="0.0.0",
+    description="ray examples",
+    python_requires="~=3.7",
+    packages=find_packages(exclude=["tests"]),
+    package_data={
+        "": ["py.typed"],
+    },
+    install_requires=["ray[data]~=1.9", "tqdm~=4.62"],
+)
