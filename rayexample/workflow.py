64ad0255134ab2086669e6d9d1909b3ea12b279f
Oliver Mannion <125105+tekumara@users.noreply.github.com>
2022-03-14 08:06:50 +0000

    feat: add basic workflow examples

---
+++ rayexample/workflow.py
@@ -0,0 +1,34 @@
+# see https://docs.ray.io/en/latest/workflows/basics.html
+
+from ray import workflow
+from typing import List
+
+@workflow.step
+def read_data(num: int):
+    return [i for i in range(num)]
+
+@workflow.step
+def preprocessing(data: List[float]) -> List[float]:
+    return [d**2 for d in data]
+
+@workflow.step
+def aggregate(data: List[float]) -> float:
+    return sum(data)
+
+# Initialize workflow storage.
+workflow.init()
+
+# Setup the workflow.
+data = read_data.step(10)
+preprocessed_data = preprocessing.step(data)
+output = aggregate.step(preprocessed_data)
+
+print(workflow.list_all())
+
+# Execute the workflow and print the result.
+print(output.run())
+
+# The workflow can also be executed asynchronously.
+# print(ray.get(output.run_async()))
+
+print(workflow.list_all())
---
+++ rayexample/workflow_metadata.py
@@ -0,0 +1,35 @@
+# see https://docs.ray.io/en/latest/workflows/metadata.html#notes
+
+from ray import workflow
+import time
+import os
+
+workflow_id = "test"
+flagfile = f"/tmp/{workflow_id}"
+
+@workflow.step
+def simple():
+    open(flagfile, 'a').close()  # touch a file here
+    time.sleep(10000)
+    return 0
+
+workflow.init()
+simple.step().run_async(workflow_id)
+
+# make sure workflow step starts running
+# while not os.path.exists(flagfile):
+#     time.sleep(1)
+
+workflow_metadata = workflow.get_metadata(workflow_id)
+print(workflow_metadata)
+assert workflow_metadata["status"] == "RUNNING"
+assert "start_time" in workflow_metadata["stats"]
+#assert "end_time" not in workflow_metadata["stats"]
+
+workflow.cancel(workflow_id)
+
+workflow_metadata = workflow.get_metadata(workflow_id)
+print(workflow_metadata)
+assert workflow_metadata["status"] == "CANCELED"
+assert "start_time" in workflow_metadata["stats"]
+#assert "end_time" not in workflow_metadata["stats"]
