f3d8b1aea1f7788c72eb0ae90cf3b10212588077
Oliver Mannion <125105+tekumara@users.noreply.github.com>
2022-05-08 20:41:48 +1000

    feat: add fault tolerance example

---
+++ rayexample/fault_tolerance.py
@@ -0,0 +1,25 @@
+import numpy as np
+import os
+import ray
+import ray.exceptions
+import time
+
+ray.init(ignore_reinit_error=True)
+
+@ray.remote(max_retries=1)
+def potentially_fail(failure_probability):
+    time.sleep(0.2)
+    if np.random.random() < failure_probability:
+        os._exit(0)
+    return 0
+
+for _ in range(3):
+    try:
+        # If this task crashes, Ray will retry it up to one additional
+        # time. If either of the attempts succeeds, the call to ray.get
+        # below will return normally. Otherwise, it will raise an
+        # exception.
+        ray.get(potentially_fail.remote(0.8))
+        print('SUCCESS')
+    except ray.exceptions.WorkerCrashedError:
+        print('FAILURE')
