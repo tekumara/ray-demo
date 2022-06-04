1118e79e0902243166e12b49805f8249af002b2d
Oliver Mannion <125105+tekumara@users.noreply.github.com>
2022-06-04 23:04:17 +1000

    chore: bump to python 3.9

--- setup.py
+++ setup.py
@@ -4,10 +4,11 @@ setup(
     name="rayexample",
     version="0.0.0",
     description="ray examples",
-    python_requires="~=3.7.7",
+    python_requires="~=3.9.11",
     packages=find_packages(exclude=["tests"]),
     package_data={
         "": ["py.typed"],
     },
-    install_requires=["ray[data]==1.12.1", "tqdm"],
+    # pin protobuf because of https://github.com/ray-project/ray/issues/25282
+    install_requires=["ray[data]==1.12.1", "tqdm", "protobuf >= 3.8.0, < 4.0.0"],
 )
