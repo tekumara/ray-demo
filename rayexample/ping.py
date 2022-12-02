12eadf48c9e3fa2e0c42df8aafff7e6ae61b119d
Oliver Mannion <125105+tekumara@users.noreply.github.com>
2022-12-02 01:46:40 +0000

    feat: ping supports cli arg

--- rayexample/ping.py
+++ rayexample/ping.py
@@ -2,16 +2,21 @@ import grpc
 import grpc._channel
 import ray.core.generated.ray_client_pb2 as ray_client_pb2
 import ray.core.generated.ray_client_pb2_grpc as ray_client_pb2_grpc
+import sys
 
-
-def ping() -> ray_client_pb2.ClusterInfoResponse:
-    with grpc.insecure_channel("localhost:10001") as channel:
+def ping(address: str) -> ray_client_pb2.ClusterInfoResponse:
+    with grpc.insecure_channel(address) as channel:
         stub = ray_client_pb2_grpc.RayletDriverStub(channel)
         return stub.ClusterInfo(ray_client_pb2.ClusterInfoRequest(type="PING"))
 
 
 try:
-    print(ping())
+    address = sys.argv[1]
+except IndexError as e:
+    address = "localhost:10001"
+
+try:
+    print(ping(address))
 except grpc._channel._InactiveRpcError as e:
     print(f"{e.code()} {e.details()}")
     exit(42)
